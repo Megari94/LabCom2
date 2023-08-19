44b1527249f0a610b354d49be56741d5ba8f57c6
unknown <marianelaholsbach@gmail.com>
2023-08-19 20:52:12 -0300

    trabajo recursividad

---
+++ src/Recursion/divisionConResta.java
@@ -0,0 +1,47 @@
+//Realizar un programa
+package Recursion;
+//USANDO RECURSIVIDAD
+/*public class divisionConResta {
+    public static int divisionConRestaRecursiva(int dividendo, int divisor) {
+        if (divisor == 0) {
+            System.out.println("No se puede dividir por cero");
+        } else if (dividendo < divisor) {
+            return 0;
+        } else {
+            return 1 + divisionConRestaRecursiva(dividendo - divisor, divisor);
+        }
+    }
+    public static void main(String[] args) {
+        int dividend = 20;
+        int divisor = 4;
+
+        int resultado = divisionConRestaRecursiva(dividend, divisor);
+        System.out.println("El resultado de la división: " + resultado);
+    }
+}*/
+
+//SIN RECURSIVIDAD
+
+public class divisionConResta {
+    public static int divisionConRestaSinRecursion(int dividendo, int divisor) {
+        if (divisor == 0) {
+            System.out.println("No se puede dividir por cero");
+
+        }
+
+        int cociente = 0;
+        while (dividendo >= divisor) {
+            dividendo -= divisor;
+            cociente++;
+        }
+        return cociente;
+    }
+
+    public static void main(String[] args) {
+        int dividendo = 20;
+        int divisor = 0;
+
+        int resultado = divisionConRestaSinRecursion(dividendo, divisor);
+        System.out.println("Resultado de la división: " + resultado);
+    }
+}
